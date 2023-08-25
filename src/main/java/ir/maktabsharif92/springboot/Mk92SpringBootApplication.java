971e8a1f48d8ffeada320232d4a1a47ef26b88a5
maxixking66 <maxixking66@gamil.com>
2023-08-25 11:11:38 +0330

    add cache for about us

--- src/main/java/ir/maktabsharif92/springboot/Mk92SpringBootApplication.java
+++ src/main/java/ir/maktabsharif92/springboot/Mk92SpringBootApplication.java
@@ -2,8 +2,10 @@ package ir.maktabsharif92.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Mk92SpringBootApplication {
 
     public static void main(String[] args) {
--- src/main/java/ir/maktabsharif92/springboot/service/impl/AboutUsServiceImpl.java
+++ src/main/java/ir/maktabsharif92/springboot/service/impl/AboutUsServiceImpl.java
@@ -4,6 +4,8 @@ import ir.maktabsharif92.springboot.domain.AboutUs;
 import ir.maktabsharif92.springboot.repository.AboutUsRepository;
 import ir.maktabsharif92.springboot.service.AboutUsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AboutUsServiceImpl implements AboutUsService {
+
+    public static final String ABOUT_US_CACHE = "ABOUT_US_CACHE";
 
     private final AboutUsRepository baseRepository;
 
@@ -27,12 +31,14 @@ public class AboutUsServiceImpl implements AboutUsService {
     }
 
     @Override
+    @Cacheable(value = ABOUT_US_CACHE)
     public AboutUs get() {
         return baseRepository.findAll().get(0);
     }
 
     @Override
     @Transactional
+    @CacheEvict(value = ABOUT_US_CACHE, allEntries = true)
     public AboutUs update(String content) {
         AboutUs aboutUs = get();
         aboutUs.setContent(content);
